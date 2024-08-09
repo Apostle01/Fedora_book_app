f5019bc4a7de6d3b06a8fc253ac01ab3458a60b1
Jewel <nanaaafianyamekye2@gmail.com>
2024-08-10 00:26:28 +0100

    Deleted website/template

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:Admin@localhost/postgres'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'#'postgresql://postgres:Admin@localhost/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
