840161b63569bc27f388f3f119abb417b1789431
Jewel <nanaaafianyamekye2@gmail.com>
2024-08-10 09:46:41 +0100

    Add env.py to .gitignore

---
+++ env.py
@@ -0,0 +1,10 @@
+import os
+ 
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "f5bc222cb7bcd4d4bc933528608bc608d3f25680723aaf60")
+os.environ.setdefault("DEBUG", "True")
+os.environ.setdefault("DEVELOPMENT", "True")
+os.environ.setdefault(
+    "DB_URL", 'postgresql://postgres:Admin@localhost/books'
+)
