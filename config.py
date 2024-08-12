d02534f9be01c02c493bdd7a4934591710d069d7
Jewel <nanaaafianyamekye2@gmail.com>
2024-08-12 09:22:06 +0100

    Switch to psycopg2 from psycopg2-binary

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'#'postgresql://postgres:Admin@localhost/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
