a63e0b819e088de2b96da0a4c15b597ea1902f64
Jewel <nanaaafianyamekye2@gmail.com>
2024-08-14 09:18:15 +0100

    Fixed SQLAlchemy database URI

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:Admin@localhost/postgres'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgresql://postgres:Admin@localhost:5432/books"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
