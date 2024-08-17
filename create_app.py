4603a9af4ca4308eb995936240228b0c6b34280c
Jewel <nanaaafianyamekye2@gmail.com>
2024-08-17 21:48:36 +0100

    Modified create_app.py

--- create_app.py
+++ create_app.py
@@ -3,15 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
 
-db = SQLAlchemy()
+db = SQLAlchemy() # Create the db instance here, not inside create_app
 login_manager = LoginManager()  # Define login_manager
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    db.init_app(app)
-    
+    db.init_app(app) # Initialize the db with the app
+
     login_manager.init_app(app)
     login_manager.init_view = 'login'
 
